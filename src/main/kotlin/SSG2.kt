a529c7f0b12774673d256a79de774fa03f17a203
gpfla6022 <gpfla3503@gmail.com>
2021-04-27 22:57:04 +0900

    Rq 클래스와 init

---
+++ src/main/kotlin/SSG2.kt
@@ -0,0 +1,29 @@
+fun main(){
+    println("SIMPLE SSG 시작 ==")
+
+    while (true){
+        print("명령어) ")
+        val command = readLineTrim()
+
+        val rq = Rq(command)
+
+        println("rq.actionPath : ${rq.actionPath}")
+        println("rq.queryStr : ${rq.queryStr}")
+    }
+
+
+    println("SIMPLE SSG 시작 ==")
+}
+
+fun readLineTrim() = readLine()!!.trim()
+
+class Rq(command: String){
+    val actionPath: String
+    val queryStr: String
+
+    init {
+        val commandBits = command.split("?", limit = 2)
+        actionPath = commandBits[0]
+        queryStr = commandBits[1]
+    }
+}
